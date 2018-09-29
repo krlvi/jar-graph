26cbab79ef470f5c04734bda3901e28803b8c135
Kiril Videlov <kiril.videlov@tink.se>
2018-09-29 20:03:13 +0200

    update release script

--- bin/ci/release.sh
+++ bin/ci/release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-mkdir -p ./release/bin
+mkdir -p ./jar-graph-release/bin
 
 #lein test;
 lein uberjar;
-mv ./target/uberjar/jar-graph-0.1.0-standalone.jar ./release/bin
-cp ./resources/jar-graph.sh ./release/
-zip -r release.zip ./release
+mv ./target/uberjar/jar-graph-0.1.0-standalone.jar ./jar-graph-release/bin
+cp ./resources/jar-graph.sh ./jar-graph-release/
+zip -r jar-graph-release.zip ./jar-graph-release
